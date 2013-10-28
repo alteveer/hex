61c54e1b7a58421d8e8683ea1794f34be7a92489
Colin Alteveer <alteveer@gmail.com>
2013-10-28 13:06:49 -0400

    initial commit

---
+++ Assets/hexmap.js
@@ -0,0 +1,58 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+//var newVertices : Vector3[];
+//var newUV : Vector2[];
+//var newTriangles : int[];
+
+var __angle: int;
+var size: int = 100;
+var height_map_z:int = 0;
+
+var x_i:float;
+var y_i:float;
+var z_i:float;
+
+var center_x:float;
+var center_y:float;
+var idx:int;
+
+var verts:Array;
+var tris:Array;
+var uvs:Array;
+
+function Update () {
+	var mesh : Mesh = GetComponent(MeshFilter).mesh;
+	mesh.Clear();
+	// Do some calculations...
+	center_x = 0;
+	center_y = 0;
+	idx = 0;
+	verts = new Array();
+	tris = new Array();
+	uvs = new Array();
+	for(var i:int = 0; i < 6; i++) {
+		__angle = ((2 * Mathf.PI) / 6) * i;
+		x_i = center_x + size * Mathf.Cos(__angle) + gameObject.transform.position.x;
+		y_i = center_y + size * Mathf.Sin(__angle) + gameObject.transform.position.y;
+		z_i = gameObject.transform.position.z;
+		verts.Push(Vector3(x_i, y_i, z_i));		
+		uvs.Push(Vector2(0, 0));
+	}
+	
+	tris.Push(0);
+	tris.Push(1);
+	tris.Push(2);
+	tris.Push(0);
+	tris.Push(3);
+	tris.Push(2);
+	
+	mesh.vertices = verts.ToBuiltin(Vector3) as Vector3[];
+	mesh.uv = uvs.ToBuiltin(Vector2) as Vector2[];
+	mesh.triangles = tris.ToBuiltin(int) as int[];
+	
+	
+}
