965d16f2df408d56d56844033d819dde73936ad5
Colin Alteveer <alteveer@gmail.com>
2013-10-28 20:03:32 -0400

    houston, we have a hexagon

--- Assets/hexmap.js
+++ Assets/hexmap.js
@@ -8,7 +8,7 @@ function Start () {
 //var newUV : Vector2[];
 //var newTriangles : int[];
 
-var __angle: int;
+var __angle: float;
 var size: int = 100;
 var height_map_z:int = 0;
 
@@ -17,7 +17,7 @@ var y_i:float;
 var z_i:float;
 
 var center_x:float;
-var center_y:float;
+var center_z:float;
 var idx:int;
 
 var verts:Array;
@@ -29,7 +29,7 @@ function Update () {
 	mesh.Clear();
 	// Do some calculations...
 	center_x = 0;
-	center_y = 0;
+	center_z = 0;
 	idx = 0;
 	verts = new Array();
 	tris = new Array();
@@ -37,18 +37,27 @@ function Update () {
 	for(var i:int = 0; i < 6; i++) {
 		__angle = ((2 * Mathf.PI) / 6) * i;
 		x_i = center_x + size * Mathf.Cos(__angle) + gameObject.transform.position.x;
-		y_i = center_y + size * Mathf.Sin(__angle) + gameObject.transform.position.y;
-		z_i = gameObject.transform.position.z;
+		y_i = 0;
+		z_i = center_z + size * Mathf.Sin(__angle) + gameObject.transform.position.z;
 		verts.Push(Vector3(x_i, y_i, z_i));		
 		uvs.Push(Vector2(0, 0));
 	}
 	
 	tris.Push(0);
-	tris.Push(1);
 	tris.Push(2);
+	tris.Push(1);
 	tris.Push(0);
 	tris.Push(3);
 	tris.Push(2);
+	tris.Push(0);
+	tris.Push(4);
+	tris.Push(3);
+	tris.Push(0);
+	tris.Push(5);
+	tris.Push(4);
+
+	
+	
 	
 	mesh.vertices = verts.ToBuiltin(Vector3) as Vector3[];
 	mesh.uv = uvs.ToBuiltin(Vector2) as Vector2[];
