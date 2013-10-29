d0080bb535b96b1aff0a53c52d573aeba4a0b90d
Colin Alteveer <alteveer@gmail.com>
2013-10-29 12:58:06 -0400

    trying to fix UVs, added grass tex

--- Assets/hexmap.js
+++ Assets/hexmap.js
@@ -24,16 +24,17 @@ var r:float;
 var idx:int;
 
 var verts:Array;
+var normals:Array;
 var tris:Array;
 var uvs:Array;
 
 var sides:int = 6;
 
 var map:Array = new Array(
-	0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 1,
 	0, 1, 0, 1, 1, 0,
-	0, 1, 1, 1, 1, 0,
-	0, 0, 1, 0, 0, 0,
+	0, 1, 1, 1, 1, 1,
+	0, 0, 1, 0, 0, 1,
 	0, 1, 1, 1, 1, 0,
 	0, 0, 1, 1, 1, 0,
 	0, 0, 0, 0, 0, 0
@@ -48,43 +49,51 @@ function Update () {
 	z = 0;
 	idx = 0;
 	verts = new Array();
+	normals = new Array();
 	tris = new Array();
 	uvs = new Array();
 	
 	for(var m:int = 0; m < map.length; m++) {
-		q = m % 6;
-		r = Mathf.FloorToInt(m / 6);
-		
-		x = q * size * 1.5;
-		z = ((r * Mathf.Sqrt(3)) + ((q % 2) * Mathf.Sqrt(3)/2)) * size; //- ((q - ()) * ( * (size * 1.5))) ;
-		y = -x - z;
-		y = gameObject.transform.position.y;
-
-		
-		
-		// center vert
-		verts.Push(Vector3(x, y, z));		
-		uvs.Push(Vector2(x % 2, z % 2));
-		
-		for(var i:int = 0; i < sides; i++) {
-			__angle = ((2 * Mathf.PI) / 6) * i;
-			x_i = x + (size * Mathf.Cos(__angle)) + gameObject.transform.position.x;
-			y_i = y + gameObject.transform.position.x;
-			z_i = z + (size * Mathf.Sin(__angle)) + gameObject.transform.position.z;
-			verts.Push(Vector3(x_i, y_i, z_i));		
-			uvs.Push(Vector2(x_i % 2, z_i % 2));
+		if(map[m] == 1) {
+			q = m % 6;
+			r = Mathf.FloorToInt(m / 6);
 			
-			tris.Push(idx);
-			if(i + 1 == sides) {
-				tris.Push(idx + 1);	
-			} else {
-				tris.Push(idx + i + 2);
+			x = -q * size * 1.5;
+			z = ((r * Mathf.Sqrt(3)) + ((q % 2) * Mathf.Sqrt(3)/2)) * size; //- ((q - ()) * ( * (size * 1.5))) ;
+			y = 0;// proper: -x - z;
+			
+			x += gameObject.transform.position.x;
+			y += gameObject.transform.position.y;
+			z += gameObject.transform.position.z;
+			
+			// center vert
+			verts.Push(Vector3(x, y, z));		
+			normals.Push(Vector3.up);
+			uvs.Push(Vector2(x % 1, z % 1));
+			
+			for(var i:int = 0; i < sides; i++) {
+				__angle = ((2 * Mathf.PI) / 6) * i;
+				x_i = x + (size * Mathf.Cos(__angle));
+				y_i = y;
+				z_i = z + (size * Mathf.Sin(__angle));
+				
+				verts.Push(Vector3(x_i, y_i, z_i));		
+				normals.Push(Vector3.up);
+				//uvs.Push(Vector2((x_i % 1) * (x_i/Mathf.Abs(x_i)), (z_i % 1) * (z_i/Mathf.Abs(z_i))));
+				uvs.Push(Vector2(Mathf.Repeat(x_i, 1), Mathf.Repeat(z_i, 1)));
+				
+				tris.Push(idx);
+				if(i + 1 == sides) {
+					tris.Push(idx + 1);	
+				} else {
+					tris.Push(idx + i + 2);
+				}
+				tris.Push(idx + i + 1);
+				
 			}
-			tris.Push(idx + i + 1);
 			
+			idx += 7;
 		}
-		
-		idx += 7;
 	}
 
 //	verts.Push(Vector3(x, y, z));		
@@ -110,8 +119,10 @@ function Update () {
 	
 	
 	mesh.vertices = verts.ToBuiltin(Vector3) as Vector3[];
+	mesh.normals = normals.ToBuiltin(Vector3) as Vector3[];
 	mesh.uv = uvs.ToBuiltin(Vector2) as Vector2[];
 	mesh.triangles = tris.ToBuiltin(int) as int[];
+	
 	
 	
 }
