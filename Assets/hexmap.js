3cb958ed5aaf98dba43fc43f3f4c896c32b175fc
Colin Alteveer <alteveer@gmail.com>
2013-10-30 12:59:08 -0400

    ported over amitp's radial mapgen, no results -- probably needs some scaling

--- Assets/hexmap.js
+++ Assets/hexmap.js
@@ -1,9 +1,5 @@
 ﻿#pragma strict
 
-function Start () {
-
-}
-
 //var newVertices : Vector3[];
 //var newUV : Vector2[];
 //var newTriangles : int[];
@@ -15,6 +11,9 @@ var height_map_z:int = 0;
 var x_i:float;
 var y_i:float;
 var z_i:float;
+
+var tiling_u:float = 1;
+var tiling_v:float = 1;
 
 var x:float;
 var y:float;
@@ -30,6 +29,9 @@ var uvs:Array;
 
 var sides:int = 6;
 
+var map_width = 40;
+var map_height = 40;
+
 var map:Array = new Array(
 	0, 0, 0, 0, 0, 1,
 	0, 1, 0, 1, 1, 0,
@@ -39,6 +41,42 @@ var map:Array = new Array(
 	0, 0, 1, 1, 1, 0,
 	0, 0, 0, 0, 0, 0
 );
+
+var ISLAND_FACTOR:float = 1.07;  // 1.0 means no small islands; 2.0 leads to a lot
+var bumps:int;
+var startAngle:float;
+var dipAngle:float;
+var dipWidth:float;
+
+function Start () {
+	bumps = Random.Range(1, 6);
+	startAngle = Random.Range(0, 2*Mathf.PI);
+	dipAngle = Random.Range(0, 2*Mathf.PI);
+	dipWidth = Random.Range(0.2, 0.7);
+	
+	map = new Array(map_width * map_height);
+	for(var m:int = 0; m < map.length; m++) {
+		map[m] = inside(m % map_width, Mathf.FloorToInt(m / map_width)) == 0 ? 1: 0;
+	}
+	
+
+}
+
+function inside(x:int, y:int) {
+  var angle:float = Mathf.Atan2(y, x);
+  var length:float = 0.5 * (Mathf.Max(Mathf.Abs(x), Mathf.Abs(y)) + Mathf.Sqrt((x*x) + (y*y)));
+
+  var r1:float = 0.5 + 0.40 * Mathf.Sin(startAngle + bumps*angle + Mathf.Cos((bumps+3)*angle));
+  var r2:float = 0.7 - 0.20 * Mathf.Sin(startAngle + bumps*angle - Mathf.Sin((bumps+2)*angle));
+  if (Mathf.Abs(angle - dipAngle) < dipWidth
+      || Mathf.Abs(angle - dipAngle + 2*Mathf.PI) < dipWidth
+      || Mathf.Abs(angle - dipAngle - 2*Mathf.PI) < dipWidth) {
+    r1 = r2 = 0.2;
+  }
+  return  (length < r1 || (length > r1*ISLAND_FACTOR && length < r2));
+}
+
+
 
 function Update () {
 	var mesh : Mesh = GetComponent(MeshFilter).mesh;
@@ -55,8 +93,8 @@ function Update () {
 	
 	for(var m:int = 0; m < map.length; m++) {
 		if(map[m] == 1) {
-			q = m % 6;
-			r = Mathf.FloorToInt(m / 6);
+			q = m % map_width;
+			r = Mathf.FloorToInt(m / map_width);
 			
 			x = -q * size * 1.5;
 			z = ((r * Mathf.Sqrt(3)) + ((q % 2) * Mathf.Sqrt(3)/2)) * size; //- ((q - ()) * ( * (size * 1.5))) ;
@@ -69,7 +107,7 @@ function Update () {
 			// center vert
 			verts.Push(Vector3(x, y, z));		
 			normals.Push(Vector3.up);
-			uvs.Push(Vector2(x % 1, z % 1));
+			uvs.Push(Vector2(x * tiling_u, z * tiling_v));
 			
 			for(var i:int = 0; i < sides; i++) {
 				__angle = ((2 * Mathf.PI) / 6) * i;
@@ -80,7 +118,7 @@ function Update () {
 				verts.Push(Vector3(x_i, y_i, z_i));		
 				normals.Push(Vector3.up);
 				//uvs.Push(Vector2((x_i % 1) * (x_i/Mathf.Abs(x_i)), (z_i % 1) * (z_i/Mathf.Abs(z_i))));
-				uvs.Push(Vector2(Mathf.Repeat(x_i, 1), Mathf.Repeat(z_i, 1)));
+				uvs.Push(Vector2(x_i * tiling_u, z_i * tiling_v));
 				
 				tris.Push(idx);
 				if(i + 1 == sides) {
@@ -96,33 +134,10 @@ function Update () {
 		}
 	}
 
-//	verts.Push(Vector3(x, y, z));		
-//	uvs.Push(Vector2(x % 2, z % 2));
-//
-//	for(var i:int = 0; i < sides; i++) {
-//		__angle = ((2 * Mathf.PI) / 6) * i;
-//		x_i = x + (size * Mathf.Cos(__angle)) + gameObject.transform.position.x;
-//		y_i = y;
-//		z_i = z + (size * Mathf.Sin(__angle)) + gameObject.transform.position.z;
-//		verts.Push(Vector3(x_i, y_i, z_i));		
-//		uvs.Push(Vector2(x_i % 2, z_i % 2));
-//		
-//		tris.Push(idx);
-//		if(i + 1 == sides) {
-//			tris.Push(idx + 1);	
-//		} else {
-//			tris.Push(idx + i + 2);
-//		}
-//		tris.Push(idx + i + 1);
-//		
-//	}
-	
-	
 	mesh.vertices = verts.ToBuiltin(Vector3) as Vector3[];
 	mesh.normals = normals.ToBuiltin(Vector3) as Vector3[];
 	mesh.uv = uvs.ToBuiltin(Vector2) as Vector2[];
 	mesh.triangles = tris.ToBuiltin(int) as int[];
 	
-	
-	
 }
+
